f1ca08b5b040503c640557fba5064485526f0552
liamkim <liamkim1018@gmail.com>
2022-01-21 15:45:14 +0900

    3장 템플릿 마무리

--- src/main/java/com/toby/spring/user/dao/UserDao.java
+++ src/main/java/com/toby/spring/user/dao/UserDao.java
@@ -1,27 +1,31 @@
 package com.toby.spring.user.dao;
 
 import com.toby.spring.user.domain.User;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class UserDao {
 
-    private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
+    private RowMapper<User> userMapper =
+        new RowMapper<User>() {
+            @Override
+            public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                User user = new User();
+                user.setId(rs.getString("id"));
+                user.setName(rs.getString("name"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            }
+        };
 
     public void setDataSource(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
-        this.dataSource = dataSource;
     }
 
     public void add(final User user) throws SQLException {
@@ -31,18 +35,11 @@ public class UserDao {
 
     public User get(String id) throws SQLException {
         return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-                new Object[] {id},
-                new RowMapper<User>() {
-                    @Override
-                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        User user = new User();
-                        user.setId(rs.getString("id"));
-                        user.setName(rs.getString("name"));
-                        user.setPassword(rs.getString("password"));
+                new Object[] {id}, this.userMapper);
+    }
 
-                        return user;
-                    }
-                });
+    public List<User> getAll() {
+        return this.jdbcTemplate.query("select * from users", this.userMapper);
     }
 
     public void deleteAll() throws SQLException {
